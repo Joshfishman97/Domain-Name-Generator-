51e9519330e0aac29e63cb61efbca91346dafe57
Joshfishman97 <fishmanjosh251@gmail.com>
2021-06-29 01:03:21 +0000

    fixed the thing

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
-let extension = ['.com','.net','.io','.us']
+let extension = [".com", ".net", ".io", ".us"];
 for (let i = 0; i < pronoun.length; i++) {
   for (let j = 0; j < adj.length; j++) {
     for (let k = 0; k < adj.length; k++) {
-      for(let e = 0; e<extension.length; e++){
-          console.log(pronoun[i] + adj[j] + noun[k] + extension[e]);
+      for (let e = 0; e < extension.length; e++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + extension[e]);
       }
     }
   }
