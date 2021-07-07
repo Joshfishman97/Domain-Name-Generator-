485b0deeffd6d5d04b60f9cb98a51fa5b3517d26
Joshfishman97 <fishmanjosh251@gmail.com>
2021-07-07 01:08:23 +0000

    another commit

--- src/app.js
+++ src/app.js
@@ -9,28 +9,32 @@ function generateLi(domain) {
   return "<LI>" + domain + "</LI>";
 }
 let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
-  let extension = [".com", ".net", ".io", ".us"];
-  
-  function generateDomains (pronounArr, adjArr, NounArr, extentionArr){
-    return "<li>Here</li>"
-  }
-window.onload = function() {
-  const unorderedList = document.getElementById("list-of-domains");
-  unorderedList.innerHTML = generateDomains(pronoun,adj,noun,extension);
-  let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
-  let extension = [".com", ".net", ".io", ".us"];
-  for (let i = 0; i < pronoun.length; i++) {
-    for (let j = 0; j < adj.length; j++) {
-      for (let k = 0; k < noun.length; k++) {
-        for (let e = 0; e < extension.length; e++) {
-          console.log(pronoun[i] + adj[j] + noun[k] + extension[e]);
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extension = [".com", ".net", ".io", ".us"];
+
+function generateDomains(pronounArr, adjArr, NounArr, extensionArr) {
+  let storageArr = [];
+  for (let i = 0; i < pronounArr.length; i++) {
+    for (let j = 0; j < adjArr.length; j++) {
+      for (let k = 0; k < NounArr.length; k++) {
+        for (let e = 0; e < extensionArr.length; e++) {
+          storageArr.push(
+            pronounArr[i] + adjArr[j] + NounArr[k] + extension[e]
+          );
         }
       }
     }
   }
+  let arrayOfLiTags = storageArr.map(function(item, index) {
+    return generateLi(item);
+  });
+  let result = arrayOfLiTags.join("");
+  return result;
+}
+window.onload = function() {
+  const unorderedList = document.getElementById("list-of-domains");
+  unorderedList.innerHTML = generateDomains(pronoun, adj, noun, extension);
+
   console.log("Hello Rigo from the console!");
 };
